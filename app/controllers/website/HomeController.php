ffb2f0b185faf6d2e816202cdc844e587114e316
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2023-10-16 08:38:29 -0300

    feat: first homepage template

--- app/controllers/website/HomeController.php
+++ app/controllers/website/HomeController.php
@@ -22,12 +22,7 @@ class HomeController extends WebsitePageController {
     public static function getHome() {
 
         // Home view
-        $pageContent = View::render('website/home', [
-            'h1' => 'What is boilerplate and why do we use it?',
-            'p' => 'In computer programming, boilerplate code or boilerplate refers to sections of code that have to be 
-            included in many places with little or no alteration. It is often used when referring to languages that are considered verbose, 
-            i.e. the programmer must write a lot of code to do minimal jobs.'
-        ]);
+        $pageContent = View::render('website/home', []);
 
         // Return page view
         return parent::getPage('Home', $pageContent);
