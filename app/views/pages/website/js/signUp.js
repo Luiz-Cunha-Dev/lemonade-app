7093662ba9c2d4f8f9d20f08cc37aad11448d873
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2023-11-01 12:01:47 -0300

    [1205848879354018] - feat: signUp function

--- app/views/pages/website/js/signUp.js
+++ app/views/pages/website/js/signUp.js
@@ -11,12 +11,25 @@ const buttonForm3 = form3.querySelector("button");
 const arrowLeftForm2 = form2.querySelector(".arrow-left");
 const arrowLeftForm3 = form3.querySelector(".arrow-left");
 
+const inputName = document.getElementById("inputName");
+const inputLastName = document.getElementById("inputLastName");
 const inputEmail = document.getElementById("inputEmail");
 const inputPassword = document.getElementById("inputPassword");
 const inputConfirmPassword = document.getElementById("inputConfirmPassword");
+const inputNickname = document.getElementById("inputNickname");
 const inputPhone = document.getElementById("inputPhone");
 const inputBirthDate = document.getElementById("inputBirthDate");
 const inputCep = document.getElementById("inputCEP");
+const inputStreet = document.getElementById("inputStreet");
+const inputNumber = document.getElementById("inputNumber");
+const inputNeighborhood = document.getElementById("inputNeighborhood");
+const inputComplement = document.getElementById("inputComplement");
+const inputCity = document.getElementById("inputCity");
+const inputState = document.getElementById("inputState");
+const alertWindow = document.getElementById("alertWindow");
+
+
+
 
 buttonForm1.addEventListener("click", (event) => {
   event.preventDefault();
@@ -28,6 +41,11 @@ buttonForm2.addEventListener("click", (event) => {
   nextFormRedirect("form2");
 });
 
+buttonForm3.addEventListener("click", async(event) => {
+  event.preventDefault();
+  await signUp();
+})
+
 arrowLeftForm2.addEventListener("click", () => {
   backFormRedirect("form2");
 });
@@ -36,18 +54,35 @@ arrowLeftForm3.addEventListener("click", () => {
   backFormRedirect("form3");
 });
 
-inputCep.addEventListener("blur", validCep);
 
-inputEmail.addEventListener("blur", validEmail);
 
-inputPhone.addEventListener("blur", validPhone);
+
+inputName.addEventListener("input", validName);
+
+inputLastName.addEventListener("input", validLastName);
+
+inputNickname.addEventListener("input", validNickName);
+
+inputCep.addEventListener("input", validCep);
+
+inputEmail.addEventListener("input", validEmail);
+
+inputPhone.addEventListener("input", validPhone);
 
 inputBirthDate.addEventListener("input", validBirthDate);
 
-inputConfirmPassword.addEventListener("blur", validPasswordMatch);
+inputConfirmPassword.addEventListener("input", validPasswordMatch);
+
+inputStreet.addEventListener("input", validStreet);
+
+inputNumber.addEventListener("input", validNumber);
+
+inputNeighborhood.addEventListener("input", validNeighborhood);
+
+inputComplement.addEventListener("input", validComplement);
 
 inputPhone.addEventListener("input", function (event) {
-  const allowedCharacters = /\d/; // Apenas dígitos são permitidos
+  const allowedCharacters = /\d/;
   const formattedValue = this.value.replace(/\D/g, "");
 
   let formato = "(##) #####-####";
@@ -60,15 +95,14 @@ inputPhone.addEventListener("input", function (event) {
         resultado += formattedValue[indice];
         indice++;
       } else {
-        break; // Evita que os caracteres de formatação sejam adicionados no final
+        break;
       }
     } else {
-      // Verifica se o último caractere do valor é um dígito
       if (
         indice === formattedValue?.length &&
         !allowedCharacters.test(element)
       ) {
-        break; // Ignora o último caractere de formatação
+        break;
       }
       resultado += element;
     }
@@ -78,7 +112,7 @@ inputPhone.addEventListener("input", function (event) {
 });
 
 inputCep.addEventListener("input", function (event) {
-  const allowedCharacters = /\d/; // Apenas dígitos são permitidos
+  const allowedCharacters = /\d/;
   const formattedValue = this.value.replace(/\D/g, "");
 
   let formato = "#####-###";
@@ -91,15 +125,14 @@ inputCep.addEventListener("input", function (event) {
         resultado += formattedValue[indice];
         indice++;
       } else {
-        break; // Evita que os caracteres de formatação sejam adicionados no final
+        break;
       }
     } else {
-      // Verifica se o último caractere do valor é um dígito
       if (
         indice === formattedValue.length &&
         !allowedCharacters.test(element)
       ) {
-        break; // Ignora o último caractere de formatação
+        break;
       }
       resultado += element;
     }
@@ -107,6 +140,9 @@ inputCep.addEventListener("input", function (event) {
 
   this.value = resultado;
 });
+
+
+
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -141,7 +177,8 @@ async function backFormRedirect(currentForm) {
     form2.classList.add("hidden");
     form1.classList.remove("left");
     form1.classList.add("center");
-  } else if (currentForm == "form3") {
+  } 
+  else if (currentForm == "form3") {
     form3.classList.remove("center");
     form3.classList.add("right");
     form2.classList.remove("hidden");
@@ -163,8 +200,201 @@ async function getAddressByZipCode(cep) {
   }
 }
 
+function validName(name) {
+  const nameRegex = /^[A-Za-z\s]+$/;
+
+  if(inputName.value === ""){
+    inputName.classList.remove("is-valid");
+    inputName.classList.remove("is-invalid");
+    return;
+  }
+
+  if (!nameRegex.test(inputName.value)) {
+    if (!inputName.classList.contains("is-invalid")) {
+      inputName.classList.add("is-invalid");
+    }
+
+    if (inputName.classList.contains("is-valid")) {
+      inputName.classList.remove("is-valid");
+    }
+  } else {
+    if (inputName.classList.contains("is-invalid")) {
+      inputName.classList.remove("is-invalid");
+    }
+    if (!inputName.classList.contains("is-valid")) {
+      inputName.classList.add("is-valid");
+    }
+  }
+}
+
+function validLastName() {
+  const lastNameRegex = /^[A-Za-z\s]+$/;
+
+  if(inputLastName.value === ""){
+    inputLastName.classList.remove("is-valid");
+    inputLastName.classList.remove("is-invalid");
+    return;
+  }
+
+  if (!lastNameRegex.test(inputLastName.value)) {
+    if (!inputLastName.classList.contains("is-invalid")) {
+      inputLastName.classList.add("is-invalid");
+    }
+
+    if (inputLastName.classList.contains("is-valid")) {
+      inputLastName.classList.remove("is-valid");
+    }
+  } else {
+    if (inputLastName.classList.contains("is-invalid")) {
+      inputLastName.classList.remove("is-invalid");
+    }
+    if (!inputLastName.classList.contains("is-valid")) {
+      inputLastName.classList.add("is-valid");
+    }
+  }
+}
+
+function validNickName() {
+  const nickNameRegex = /^[A-Za-z0-9\s]+$/;
+
+  if (inputNickname.value === "") {
+    inputNickname.classList.remove("is-valid");
+    inputNickname.classList.remove("is-invalid");
+    return;
+  }
+
+  if (!nickNameRegex.test(inputNickname.value)) {
+    if (!inputNickname.classList.contains("is-invalid")) {
+      inputNickname.classList.add("is-invalid");
+    }
+
+    if (inputNickname.classList.contains("is-valid")) {
+      inputNickname.classList.remove("is-valid");
+    }
+  } else {
+    if (inputNickname.classList.contains("is-invalid")) {
+      inputNickname.classList.remove("is-invalid");
+    }
+    if (!inputNickname.classList.contains("is-valid")) {
+      inputNickname.classList.add("is-valid");
+    }
+  }
+}
+
+function validStreet() {
+  const streetRegex = /^[A-Za-z0-9\s]+$/;
+
+  if (inputStreet.value === "") {
+    inputStreet.classList.remove("is-valid");
+    inputStreet.classList.remove("is-invalid");
+    return;
+  }
+
+  if (!streetRegex.test(inputStreet.value)) {
+    if (!inputStreet.classList.contains("is-invalid")) {
+      inputStreet.classList.add("is-invalid");
+    }
+
+    if (inputStreet.classList.contains("is-valid")) {
+      inputStreet.classList.remove("is-valid");
+    }
+  } else {
+    if (inputStreet.classList.contains("is-invalid")) {
+      inputStreet.classList.remove("is-invalid");
+    }
+    if (!inputStreet.classList.contains("is-valid")) {
+      inputStreet.classList.add("is-valid");
+    }
+  }
+}
+
+function validNumber() {
+  const numberRegex = /^\d+$/;
+
+  if (inputNumber.value === "") {
+    inputNumber.classList.remove("is-valid");
+    inputNumber.classList.remove("is-invalid");
+    return;
+  }
+
+  if (!numberRegex.test(inputNumber.value)) {
+    if (!inputNumber.classList.contains("is-invalid")) {
+      inputNumber.classList.add("is-invalid");
+    }
+
+    if (inputNumber.classList.contains("is-valid")) {
+      inputNumber.classList.remove("is-valid");
+    }
+  } else {
+    if (inputNumber.classList.contains("is-invalid")) {
+      inputNumber.classList.remove("is-invalid");
+    }
+    if (!inputNumber.classList.contains("is-valid")) {
+      inputNumber.classList.add("is-valid");
+    }
+  }
+}
+
+function validNeighborhood() {
+  const neighborhoodRegex = /^[A-Za-z0-9\s]+$/;
+
+  if (inputNeighborhood.value === "") {
+    inputNeighborhood.classList.remove("is-valid");
+    inputNeighborhood.classList.remove("is-invalid");
+    return;
+  }
+
+  if (!neighborhoodRegex.test(inputNeighborhood.value)) {
+    if (!inputNeighborhood.classList.contains("is-invalid")) {
+      inputNeighborhood.classList.add("is-invalid");
+    }
+
+    if (inputNeighborhood.classList.contains("is-valid")) {
+      inputNeighborhood.classList.remove("is-valid");
+    }
+  } else {
+    if (inputNeighborhood.classList.contains("is-invalid")) {
+      inputNeighborhood.classList.remove("is-invalid");
+    }
+    if (!inputNeighborhood.classList.contains("is-valid")) {
+      inputNeighborhood.classList.add("is-valid");
+    }
+  }
+}
+
+function validComplement() {
+  const complementRegex = /^[A-Za-z0-9\s]+$/;
+
+  if (inputComplement.value === "") {
+    inputComplement.classList.remove("is-valid");
+    inputComplement.classList.remove("is-invalid");
+    return;
+  }
+
+  if (!complementRegex.test(inputComplement.value)) {
+    if (!inputComplement.classList.contains("is-invalid")) {
+      inputComplement.classList.add("is-invalid");
+    }
+
+    if (inputComplement.classList.contains("is-valid")) {
+      inputComplement.classList.remove("is-valid");
+    }
+  } else {
+    if (inputComplement.classList.contains("is-invalid")) {
+      inputComplement.classList.remove("is-invalid");
+    }
+    if (!inputComplement.classList.contains("is-valid")) {
+      inputComplement.classList.add("is-valid");
+    }
+  }
+}
+
 async function validCep() {
-  if (inputCep.value.length === 0) return;
+  if (inputCep.value.length === 0){
+    inputCep.classList.remove("is-valid");
+    inputCep.classList.remove("is-invalid");
+    return;
+  }
 
   if (inputCep.value.length < 8) {
     if (!inputCep.classList.contains("is-invalid")) {
@@ -182,7 +412,9 @@ async function validCep() {
 
   if (address && !address.erro) {
     document.getElementById("inputStreet").value = address.logradouro;
+    validStreet();
     document.getElementById("inputNeighborhood").value = address.bairro;
+    validNeighborhood();
     document.getElementById("inputCity").value = address.localidade;
     document.getElementById("inputState").value = address.uf;
 
@@ -207,22 +439,26 @@ async function validCep() {
 function validEmail() {
   const regexEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-  if (inputEmail.value.length === 0) return;
+  if (inputEmail.value.length === 0){
+    inputEmail.classList.remove("is-valid");
+    inputEmail.classList.remove("is-invalid");
+    return;
+  }
 
   if (!regexEmail.test(inputEmail.value)) {
-    if (!regexEmail.classList.contains("is-invalid")) {
-      regexEmail.classList.add("is-invalid");
+    if (!inputEmail.classList.contains("is-invalid")) {
+      inputEmail.classList.add("is-invalid");
     }
 
-    if (regexEmail.classList.contains("is-valid")) {
-      regexEmail.classList.remove("is-valid");
+    if (inputEmail.classList.contains("is-valid")) {
+      inputEmail.classList.remove("is-valid");
     }
   } else {
-    if (regexEmail.classList.contains("is-invalid")) {
-      regexEmail.classList.remove("is-invalid");
+    if (inputEmail.classList.contains("is-invalid")) {
+      inputEmail.classList.remove("is-invalid");
     }
-    if (!regexEmail.classList.contains("is-valid")) {
-      regexEmail.classList.add("is-valid");
+    if (!inputEmail.classList.contains("is-valid")) {
+      inputEmail.classList.add("is-valid");
     }
   }
 }
@@ -230,7 +466,11 @@ function validEmail() {
 function validPhone() {
   const regexTelefone = /^\(\d{2}\) \d{5}-\d{4}$/;
 
-  if (inputPhone.value.length === 0) return;
+  if (inputPhone.value.length === 0){
+    inputPhone.classList.remove("is-valid");
+    inputPhone.classList.remove("is-invalid");
+    return;
+  }
 
   if (!regexTelefone.test(inputPhone.value)) {
     if (!inputPhone.classList.contains("is-invalid")) {
@@ -285,7 +525,13 @@ function validPasswordMatch() {
   const password = inputPassword.value;
   const confirmPassword = inputConfirmPassword.value;
 
-  if (confirmPassword.length === 0) return;
+  if (confirmPassword.length === 0){
+    inputPassword.classList.remove("is-valid");
+    inputPassword.classList.remove("is-invalid");
+    inputConfirmPassword.classList.remove("is-valid");
+    inputConfirmPassword.classList.remove("is-invalid");
+    return;
+  }
 
   if (password.length >= 5 && password === confirmPassword) {
     if (inputPassword.classList.contains("is-invalid")) {
@@ -319,5 +565,61 @@ function validPasswordMatch() {
     if (inputConfirmPassword.classList.contains("is-valid")) {
       inputConfirmPassword.classList.remove("is-valid");
     }
+  }
+}
+
+async function signUp() {
+  const user = {
+    name: inputName.value,
+    email: inputEmail.value,
+    password: inputPassword.value,
+    phone: inputPhone.value,
+    birthDate: inputBirthDate.value,
+    cep: inputCep.value,
+    street: inputStreet.value,
+    number: inputNumber.value,
+    complement: inputComplement.value,
+    neighborhood: inputNeighborhood.value,
+    city: inputCity.value,
+    state: inputState.value,
+  };
+
+  if (
+    inputName.classList.contains("is-valid") &&
+    inputLastName.classList.contains("is-valid") &&
+    inputBirthDate.classList.contains("is-valid") &&
+    inputCep.classList.contains("is-valid") &&
+    inputComplement.classList.contains("is-valid") &&
+    inputLastName.classList.contains("is-valid") &&
+    inputName.classList.contains("is-valid") &&
+    inputPassword.classList.contains("is-valid") &&
+    inputConfirmPassword.classList.contains("is-valid") &&
+    inputPhone.classList.contains("is-valid") &&
+    inputStreet.classList.contains("is-valid") &&
+    inputNumber.classList.contains("is-valid") &&
+    inputNeighborhood.classList.contains("is-valid") &&
+    inputCity.value !== "-" &&
+    inputState.value !== "-"
+  ) {
+    try {
+      buttonForm3.innerHTML = `
+      <div class="spinner-border" role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>`
+
+      await sleep(5000);
+
+      buttonForm3.innerText = "Enviar"
+      // const response = await axios.post('http://localhost:3000/users', user);
+      // console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  } else {
+   const message =  alertWindow.querySelector(".toast-body");
+    alertWindow.classList.add("show");
+    message.textContent = "Preencha todos os campos corretamente para continuar!"
+    await sleep(5000);
+    alertWindow.classList.remove("show");
   }
 }
