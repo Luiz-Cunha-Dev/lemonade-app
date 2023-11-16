f8223f5611c2043416fc62d45bc6f7cf2045fe6a
Bruno da Costa Calegari <109561086+bccalegari@users.noreply.github.com>
2023-11-16 18:07:47 -0300

    [1205954148660247] - style: questionController

--- app/controllers/webapp/QuestionController.php
+++ app/controllers/webapp/QuestionController.php
@@ -4,18 +4,9 @@ namespace app\controllers\webapp;
 
 use app\services\QuestionService;
 
-class QuestionController{
+class QuestionController {
 
-    public static function getQuestionAlternativesByIdQuestion($idQuestion){
-
-        $questionService = new QuestionService();
-
-        $alternatives =   $questionService->getQuestionAlternativesByIdQuestion($idQuestion);
-
-        return $alternatives;
-    }
-
-    public static function getQuestionById($idQuestion){
+    public static function getQuestionById($idQuestion) {
 
         $questionService = new QuestionService();
 
@@ -24,13 +15,22 @@ class QuestionController{
         return $question;
     }
 
-    public static function getQuestionTextsByIdQuestion($idQuestion){
+    public static function getQuestionTextsByIdQuestion($idQuestion) n{
 
         $questionService = new QuestionService;
 
         $texts = $questionService->getQuestionTextsByIdQuestion($idQuestion);
 
         return $texts;
+    }
+
+    public static function getQuestionAlternativesByIdQuestion($idQuestion) {
+
+        $questionService = new QuestionService();
+
+        $alternatives = $questionService->getQuestionAlternativesByIdQuestion($idQuestion);
+
+        return $alternatives;
     }
 
 }
