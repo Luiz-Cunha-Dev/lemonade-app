1dc6dc79f144d9e9021776c845a68985f7ab37d9
Bruno da Costa Calegari <109561086+bccalegari@users.noreply.github.com>
2023-11-16 18:16:25 -0300

    [1205954148660247] - fix: QuestionDAO

--- app/daos/QuestionDAO.php
+++ app/daos/QuestionDAO.php
@@ -6,42 +6,41 @@ use app\models\QuestionModel;
 use Exception;
 
 /**
- * Question alternative DAO
+ * Question DAO
  * 
  * Responsible for reading and writing data of question entity
  * 
  * @package app\daos
  */ 
-class QuestionDAO extends AbstractDAO{
+class QuestionDAO extends AbstractDAO {
 
-/**
- * Get question by id
- * 
- * @param integer id question
- * 
- * if its null, returns empty array
- * 
- * @return QuestionModel question
- */
-public function getQuestionById($idQuestion){
-
-    try {
-
-        $question = parent::getElementByParameter('question', 'idQuestion', $idQuestion);
-
-        if(empty($question)){
-            return array();
+    /**
+     * Get question by id
+     * 
+     * @param integer id question
+     * 
+     * if its null, returns empty array
+     * 
+     * @return QuestionModel question
+     */
+    public function getQuestionById($idQuestion) {
+    
+        try {
+    
+            $question = parent::getElementByParameter('question', 'idQuestion', $idQuestion);
+    
+            if(empty($question)){
+                return array();
+            }
+    
+            $question = new QuestionModel($question['idQuestion'], $question['statement'], $question['idQuestionType']);
+    
+            return $question;
+    
+        } catch (Exception $e) {
+            throw $e;
         }
-
-        $question = new QuestionModel($question['idQuestion'], $question['statement'], $question['idQuestionType']);
-
-        return $question;
-
-    } catch (\Exception $e) {
-        throw new Exception();
+        
     }
-}
-
-
 
 }
