abd8aed9cb9f5991d5b19bdded45c2d34d8ea7ce
Felipesdr <felipederose@gmail.com>
2023-11-25 10:52:22 -0300

    [1205954148660258] - feat: Add UserPracticeExamQuestionDAO and methods

---
+++ app/daos/UserPracticeExamQuestionAlternativeDAO.php
@@ -0,0 +1,50 @@
+<?php 
+
+namespace app\daos;
+
+use app\models\UserPracticeExamQuestionAlternativeModel;
+use Exception;
+
+class UserPracticeExamQuestionAlternativeDAO extends AbstractDAO{
+
+    /**
+     * Get user question alternatives by id user practice exam and id question alternative  
+     * 
+     * @param integer id user practice exam
+     * 
+     * If it is null, returns an empty array
+     * 
+     * @return UserPracticeExamQuestionAlternative user practice exam
+     */
+    public function getUserPracticeExamQuestionAlternatives ($idUserPracticeExam){
+
+        try {
+            $userQuestionAlternatives = parent::getElementsByParameter('userPracticeExamQuestionAlternative', 'idUserPracticeExam', $idUserPracticeExam);
+
+            if (empty($userQuestionAlternatives)){
+                return array();
+            }
+
+            for ($i = 0; $i <= count($userQuestionAlternatives); $i++){
+
+                $userQuestionAlternatives = new UserPracticeExamQuestionAlternativeModel(
+                    $userQuestionAlternatives[$i]['idUserPracticeExam'],
+                    $userQuestionAlternatives[$i]['idQuestionAlternative']);
+            }
+
+            return $userQuestionAlternatives;
+        } catch (Exception $e) {
+            throw $e;
+        }
+    }
+
+    public function insertUserPracticeExamQuestionAlternatives($userQuestionAlternatives){
+
+        try {
+            return parent::insertElement('userPracticeExamQuestionAlternative', $userQuestionAlternatives);
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+    }
+}
