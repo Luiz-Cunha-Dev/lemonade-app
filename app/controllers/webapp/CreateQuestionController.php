0874f070f3d43bc07fe713875022ade1fec80ed9
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2023-12-12 23:57:54 -0300

    feat: create question and create exam page layout

---
+++ app/controllers/webapp/CreateQuestionController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace app\controllers\webapp;
+
+use app\controllers\AbstractPageController;
+use app\session\Session;
+use app\views\View;
+
+/**
+ * CreateQuestion controller
+ * 
+ * HTML file: ./view/pages/webapp/CreateQuestion/main.html
+ * CSS file: ./views/pages/style/CreateQuestion.css
+ * JS file: ./views/pages/js/dist/CreateQuestion.js
+ * 
+ * @package app\controllers\webapp
+ */ 
+class CreateQuestionController extends AbstractPageController {
+
+    private static function getStudentSidebarItems() {
+
+        $item1 = View::render('pages/components/sidebarItem', [
+            'itemUrl' => 'wapp/ranking',
+            'imageUrl' => './app/views/pages/assets/svgs/ranking.svg',
+            'itemName' => 'Ranking'
+        ]);
+
+        $item2 = View::render('pages/components/sidebarItem', [
+            'itemUrl' => 'wapp/exam',
+            'imageUrl' => './app/views/pages/assets/svgs/exam.svg',
+            'itemName' => 'Simulados'
+        ]);
+
+        $item3 = View::render('pages/components/sidebarItem', [
+            'itemUrl' => 'wapp/train',
+            'imageUrl' => './app/views/pages/assets/svgs/target.svg',
+            'itemName' => 'Treinar'
+        ]);
+
+        return $item1 . $item2 . $item3;
+    }
+
+    private static function getAdminSidebarItems() {
+        
+        $item1 = View::render('pages/components/sidebarItem', [
+            'itemUrl' => 'wapp/ranking',
+            'imageUrl' => './app/views/pages/assets/svgs/ranking.svg',
+            'itemName' => 'Ranking'
+        ]);
+
+        $item2 = View::render('pages/components/sidebarItem', [
+            'itemUrl' => 'wapp/exam',
+            'imageUrl' => './app/views/pages/assets/svgs/exam.svg',
+            'itemName' => 'Simulados'
+        ]);
+
+        $item3 = View::render('pages/components/sidebarItem', [
+            'itemUrl' => 'wapp/users',
+            'imageUrl' => './app/views/pages/assets/svgs/users.svg',
+            'itemName' => 'Usuários'
+        ]);
+
+        $item4 = View::render('pages/components/sidebarItem', [
+            'itemUrl' => 'wapp/questions',
+            'imageUrl' => './app/views/pages/assets/svgs/book.svg',
+            'itemName' => 'Questões'
+        ]);
+
+        return $item1 . $item2 . $item3 . $item4;
+    }
+
+    /**
+     * Return the content of app CreateQuestion view
+     * 
+     * @return string app CreateQuestion rendered page
+     */
+    public static function getCreateQuestion() {
+
+        // App CreateQuestion view
+
+        $header = View::render('pages/webapp/html/header', [
+            'userName' => Session::getCurrentUserSessionData()['name'],
+            'lastName' => Session::getCurrentUserSessionData()['lastName'],
+            'sidebarItems' => Session::getCurrentUserSessionData()['userType'] == 1 ? self::getStudentSidebarItems() : self::getAdminSidebarItems(),
+            'profilePicture' => Session::getCurrentUserSessionData()['profilePicture']
+        ]);
+
+        $main = View::render('pages/webapp/html/createQuestion/main', [
+            'nickname' => Session::getCurrentUserSessionData()['nickname'],
+        ]);
+
+        $footer = View::render('pages/webapp/html/footer');
+
+        // Return page view
+
+        return parent::getPage('App', $header, $main, $footer, 
+        ['css' => './app/views/pages/style/createQuestion.css', 'js' => './app/views/pages/js/dist/createQuestion.js']);
+    }
+
+}
